4cbf2203d0599d619b7cf0061d9bcb2e6efe63c5
Sudarshana Dayananda <sudarshana@virtualcapital.co.nz>
2021-01-30 12:02:23 +0530

    create and delete friends

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+interface IFriend {
+  id: number;
+  name: string;
+  age: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'frontend';
+  name: string;
+  age: number;
+  friendList: Array<IFriend>;
+
+  constructor() {
+    this.friendList = [];
+  }
+
+  addFriend(): void {
+    const friend = {
+      id: this.friendList.length,
+      name: this.name,
+      age: this.age,
+    };
+    this.friendList.push(friend);
+    this.name = null;
+    this.age = null;
+  }
+
+  deleteFriend(id: number): void {
+    this.friendList.splice(id, 1);
+  }
 }
